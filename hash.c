4c01f5cc7ec152e5e0a98694db0babf0e497a28d
Roberto Anic Banic <roberto@aconno.de>
2022-08-12 12:14:02 +0200

    Implement EVP OpenSSL SHA checksum generation

--- hash.c
+++ hash.c
@@ -27,7 +27,15 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 #include <inttypes.h>     /* PRId64 etc. */
 
 #ifdef USE_OPENSSL
-#include <openssl/sha.h>  /* SHA1() */
+
+#ifndef USE_OPENSSL_DEPRECATED
+
+#include <openssl/evp.h>  /* EVP* */
+
+#endif
+
+#include <openssl/sha.h>  /* SHA_DIGEST_LENGTH */
+
 #else
 #include "sha1.h"
 #endif
@@ -55,91 +63,118 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
  * concatenation of the (20 byte) SHA1 hash of every piece
  * last piece may be shorter
  */
-EXPORT unsigned char *make_hash(struct metafile *m)
-{
-	unsigned char *hash_string;     /* the hash string */
-	unsigned char *pos;             /* position in the hash string */
-	unsigned char *read_buf;        /* read buffer */
-	int fd;                         /* file descriptor */
-	size_t r;                       /* number of bytes read from file(s) into
+EXPORT unsigned char *make_hash(struct metafile *m) {
+    unsigned char *hash_string;     /* the hash string */
+    unsigned char *pos;             /* position in the hash string */
+    unsigned char *read_buf;        /* read buffer */
+    int fd;                         /* file descriptor */
+    size_t r;                       /* number of bytes read from file(s) into
 	                                   the read buffer */
-	SHA_CTX c;                      /* SHA1 hashing context */
+#ifndef USE_OPENSSL_DEPRECATED
+    EVP_MD_CTX *ctx;                /* OpenSSL hashing context */
+#else
+    SHA_CTX c;                      /* OpenSSL hashing context */
+#endif
 #ifndef NO_HASH_CHECK
-	uintmax_t counter = 0;          /* number of bytes hashed
+    uintmax_t counter = 0;          /* number of bytes hashed
 	                                   should match size when done */
 #endif
 
-	/* allocate memory for the hash string
-	   every SHA1 hash is SHA_DIGEST_LENGTH (20) bytes long */
-	hash_string = malloc(m->pieces * SHA_DIGEST_LENGTH);
-	/* allocate memory for the read buffer to store 1 piece */
-	read_buf = malloc(m->piece_length);
-
-	/* check if we've run out of memory */
-	FATAL_IF0(hash_string == NULL || read_buf == NULL, "out of memory\n");
-
-	/* initiate pos to point to the beginning of hash_string */
-	pos = hash_string;
-	/* and initiate r to 0 since we haven't read anything yet */
-	r = 0;
-	/* go through all the files in the file list */
-	LL_FOR(file_node, m->file_list) {
-		struct file_data *f = LL_DATA_AS(file_node, struct file_data*);
-
-		/* open the current file for reading */
-		FATAL_IF((fd = open(f->path, OPENFLAGS)) == -1,
-			"cannot open '%s' for reading: %s\n", f->path, strerror(errno));
-		printf("hashing %s\n", f->path);
-		fflush(stdout);
-
-		/* fill the read buffer with the contents of the file and append
-		   the SHA1 hash of it to the hash string when the buffer is full.
-		   repeat until we can't fill the read buffer and we've thus come
-		   to the end of the file */
-		while (1) {
-			ssize_t d = read(fd, read_buf + r, m->piece_length - r);
-			FATAL_IF(d < 0, "cannot read from '%s': %s\n",
-				f->path, strerror(errno));
-
-			if (d == 0) /* end of file */
-				break;
-
-			r += d;
-
-			if (r == m->piece_length) {
-				SHA1_Init(&c);
-				SHA1_Update(&c, read_buf, m->piece_length);
-				SHA1_Final(pos, &c);
-				pos += SHA_DIGEST_LENGTH;
-#ifndef NO_HASH_CHECK
-				counter += r;	/* r == piece_length */
-#endif
-				r = 0;
-			}
-		}
-
-		/* now close the file */
-		FATAL_IF(close(fd), "cannot close '%s': %s\n",
-			f->path, strerror(errno));
-	}
-
-	/* finally append the hash of the last irregular piece to the hash string */
-	if (r) {
-		SHA1_Init(&c);
-		SHA1_Update(&c, read_buf, r);
-		SHA1_Final(pos, &c);
-	}
-
-#ifndef NO_HASH_CHECK
-	counter += r;
-	FATAL_IF(counter != m->size,
-		"counted %" PRIuMAX " bytes, but hashed %" PRIuMAX " bytes; "
-		"something is wrong...\n",
-			m->size, counter);
+#ifndef USE_OPENSSL_DEPRECATED
+    /* initialize EVP context */
+    ctx = EVP_MD_CTX_new();
 #endif
 
-	/* free the read buffer before we return */
-	free(read_buf);
+    /* allocate memory for the hash string
+       every SHA1 hash is SHA_DIGEST_LENGTH (20) bytes long */
+    hash_string = malloc(m->pieces * SHA_DIGEST_LENGTH);
+    /* allocate memory for the read buffer to store 1 piece */
+    read_buf = malloc(m->piece_length);
 
-	return hash_string;
+    /* check if we've run out of memory */
+    FATAL_IF0(hash_string == NULL || read_buf == NULL, "out of memory\n");
+
+    /* initiate pos to point to the beginning of hash_string */
+    pos = hash_string;
+    /* and initiate r to 0 since we haven't read anything yet */
+    r = 0;
+    /* go through all the files in the file list */
+    LL_FOR(file_node, m->file_list) {
+        struct file_data *f = LL_DATA_AS(file_node, struct file_data*);
+
+        /* open the current file for reading */
+        FATAL_IF((fd = open(f->path, OPENFLAGS)) == -1,
+                 "cannot open '%s' for reading: %s\n", f->path, strerror(errno));
+        printf("hashing %s\n", f->path);
+        fflush(stdout);
+
+        /* fill the read buffer with the contents of the file and append
+           the SHA1 hash of it to the hash string when the buffer is full.
+           repeat until we can't fill the read buffer and we've thus come
+           to the end of the file */
+        while (1) {
+            ssize_t d = read(fd, read_buf + r, m->piece_length - r);
+            FATAL_IF(d < 0, "cannot read from '%s': %s\n",
+                     f->path, strerror(errno));
+
+            if (d == 0) /* end of file */
+                break;
+
+            r += d;
+
+            if (r == m->piece_length) {
+                unsigned int length_written = 0;
+#ifndef USE_OPENSSL_DEPRECATED
+                EVP_DigestInit_ex(ctx, EVP_sha1(), NULL);
+                EVP_DigestUpdate(ctx, read_buf, m->piece_length);
+                EVP_DigestFinal_ex(ctx, pos, &length_written);
+                pos += length_written;
+#else
+                SHA1_Init(&c);
+                SHA1_Update(&c, read_buf, m->piece_length);
+                SHA1_Final(pos, &c);
+                pos += SHA_DIGEST_LENGTH;
+#endif
+#ifndef NO_HASH_CHECK
+                counter += r;	/* r == piece_length */
+#endif
+                r = 0;
+            }
+        }
+
+        /* now close the file */
+        FATAL_IF(close(fd), "cannot close '%s': %s\n",
+                 f->path, strerror(errno));
+    }
+
+    /* finally append the hash of the last irregular piece to the hash string */
+    if (r) {
+        unsigned int length_written = 0;
+#ifndef USE_OPENSSL_DEPRECATED
+        EVP_DigestInit_ex(ctx, EVP_sha1(), NULL);
+        EVP_DigestUpdate(ctx, read_buf, m->piece_length);
+        EVP_DigestFinal_ex(ctx, pos, &length_written);
+#else
+        SHA1_Init(&c);
+        SHA1_Update(&c, read_buf, r);
+        SHA1_Final(pos, &c);
+#endif
+    }
+
+#ifndef NO_HASH_CHECK
+    counter += r;
+    FATAL_IF(counter != m->size,
+        "counted %" PRIuMAX " bytes, but hashed %" PRIuMAX " bytes; "
+        "something is wrong...\n",
+            m->size, counter);
+#endif
+
+    /* free the read buffer before we return */
+    free(read_buf);
+    /* free the OpenSSL context */
+#ifndef USE_OPENSSL_DEPRECATED
+    EVP_MD_CTX_free(ctx);
+#endif
+
+    return hash_string;
 }
